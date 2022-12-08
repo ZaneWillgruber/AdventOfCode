a9bafe92097bafa92dff47358e8d7e475e229118
ZaneWillgruber <zanetwill@gmail.com>
2022-12-08 15:47:32 -0600

    Day 1 Part 2

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -7,17 +7,25 @@ def main():
 
     max = 0
     total = 0
+    top3 = [0, 0, 0]
 
     for line in lines:
         if line != "\n":
             total += int(line)
         else:
-            if total > max:
+            if total > min(top3):
                 max = total
+                top3.sort()
+                top3[0] = max
             total = 0
+            
 
-        
+    top3total = 0
+    for i in top3:
+        top3total += i
+
     print(max)
+    print(top3total)
 
 if __name__ == "__main__":
     main()
