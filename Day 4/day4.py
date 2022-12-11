08ad722db6495dc2af7511be9cc57d4bef70319a
ZaneWillgruber <zanetwill@gmail.com>
2022-12-11 16:41:27 -0600

    Day 4

---
+++ Day 4/day4.py
@@ -0,0 +1,27 @@
+def main():
+    #read the input file
+    f = open("Day 4/input.txt", "r")
+    lines = f.readlines()
+
+    overlapCount = 0
+
+    for line in lines:
+        line = line.strip()
+        line = line.split(",")
+
+        elf1 = line[0].split("-")
+        elf2 = line[1].split("-")
+
+        #convert to ints
+        elf1 = [int(x) for x in elf1]
+        elf2 = [int(x) for x in elf2]
+
+        if elf1[0] <= elf2[0] and elf1[1] >= elf2[1]:
+            overlapCount += 1
+        elif elf2[0] <= elf1[0] and elf2[1] >= elf1[1]:
+            overlapCount += 1
+
+    print(overlapCount)
+
+if __name__ == "__main__":
+    main()
