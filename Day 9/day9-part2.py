3754d071a5fc7889c013d67b8e2462d8a247a8b9
Zane Willgruber <zane.willgruber890@topper.wku.edu>
2022-12-15 11:12:59 -0600

    Day 9

---
+++ Day 9/day9-part2.py
@@ -0,0 +1,56 @@
+def main():
+    # Read input
+    f = open("AdventOfCode2022/Day 9/input.txt", "r")
+    lines = f.readlines()
+
+    visited = []
+    tail = {'x': 0, 'y': 0}
+    knots = []
+    for i in range(8):
+        knots.append({'x': 0, 'y': 0})
+    head = {'x': 0, 'y': 0}
+
+    for line in lines:
+        line = line.strip()
+        line = line.split(" ")
+
+        for i in range(int(line[1])):
+            head = moveHead(head, line[0])
+            for j in range(len(knots)):
+                knots[j] = moveTail(knots[j], head if j == 0 else knots[j - 1])
+            tail = moveTail(tail, knots[7])
+            if (tail['x'], tail['y']) not in visited:
+                visited.append((tail['x'], tail['y']))
+
+    print(len(visited))
+
+def moveHead(head, direction):
+    if direction == "U":
+        head['y'] += 1
+    elif direction == "D":
+        head['y'] -= 1
+    elif direction == "L":
+        head['x'] -= 1
+    elif direction == "R":
+        head['x'] += 1
+    return head
+
+def moveTail(tail, head):
+    if tail['x'] == head['x'] and tail['y'] == head['y']:
+        return tail
+    else:
+        if abs(tail['x'] - head['x']) <= 1 and abs(tail['y'] - head['y']) <= 1:
+            return tail
+
+        if tail['x'] < head['x']:
+            tail['x'] += 1
+        elif tail['x'] > head['x']:
+            tail['x'] -= 1
+        if tail['y'] < head['y']:
+            tail['y'] += 1
+        elif tail['y'] > head['y']:
+            tail['y'] -= 1
+        return tail
+
+if __name__ == '__main__':
+    main()
